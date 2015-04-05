5d9817a4673677239981b9b7eeccfdd1c27c3f71
Steen Klingberg <steen@sk-ab.se>
2015-04-05 07:46:53 +0200

    cleanup

---
+++ scripts/build.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+global.window = {};
+global.document = {querySelectionAll: function () {}};
+var td = require('../src/tripledollar')
+ ,  fs = require('fs')
+ ,  uglify = require('uglify-js');
+
+var comment = "/* tripledollar v." + td.version + 
+  ", (c) " + (1900 + (new Date).getYear()) + " Steen Klingberg. License MIT. */\n";
+
+var code = fs.readFileSync('./src/tripledollar.js', 'utf8');
+var ast = uglify.parse(code);
+ast.figure_out_scope();
+ast.compute_char_frequency();
+ast.mangle_names();
+fs.writeFileSync('./tripledollar.js', comment + ast.print_to_string());
+

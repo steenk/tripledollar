9f77866eba98780193b61ad7b67e774c5ad061e7
Steen Klingberg <steen@sk-ab.se>
2020-01-20 10:33:11 +0100

    default changed

--- src/tripledollar.js
+++ src/tripledollar.js
@@ -23,7 +23,7 @@
     /**
      * @version
      */   
-    var VERSION = '1.5.8';
+    var VERSION = '1.6.0';
 
     var tripledollar = function (window) {
 
--- src/tripledollar.mjs
+++ src/tripledollar.mjs
@@ -22,7 +22,7 @@
     /**
      * @version
      */   
-    let VERSION = '1.5.8';
+    let VERSION = '1.6.0';
 
 
 
